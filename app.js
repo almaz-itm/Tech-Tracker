a104b5367f45c2c8b57b0cb9291e2a7d8c9a93d7
almaz-itm <andrew.solid@yandex.ru>
2025-06-29 16:41:34 +0500

    commit 6

--- app.js
+++ app.js
@@ -2,23 +2,56 @@ const modal = document.querySelector('#modal')
 const content = document.querySelector('#content')
 const backdrop = document.querySelector('#backdrop')
 const progress = document.querySelector('#progress')
+const form = document.querySelector('#form')
 
 content.addEventListener('click', openCard)
 backdrop.addEventListener('click', closeModal)
+modal.addEventListener('change', toggleTech)
+form.addEventListener('submit', createTech)
 
-const technologies = [
-  {title: 'HTML', description: 'HTML Text', type: 'html', done: true},
-  {title: 'CSS', description: 'CSS Text', type: 'css', done: true},
-  {title: 'JavaScript', description: 'JavaScript Text', type: 'js', done: true},
-  {title: 'Git', description: 'Git Text', type: 'git', done: false},
-  {title: 'React', description: 'React Text', type: 'react', done: false}
-]
+const APP_TITLE = document.title
+const LS_KEY = 'MY_TECHS'
 
-function openCard() {
+const technologies = getState()
+
+
+function openCard(event) {
+  const data = event.target.dataset
+  const tech = technologies.find(t => t.type === data.type)
+  if (!tech) return
+  openModal(toModal(tech), tech.title)
+}
+
+function toModal(tech) {
+  const checked = tech.done ? `checked` : ``
+  return `
+    <h2>${tech.title}</h2>
+      <p>${tech.description}</p>
+      <hr>
+      <div>
+        <input type="checkbox" id="done" ${checked} data-type="${tech.type}">
+        <label for="done">Выучил</label>
+      </div>
+  `
+}
+
+function toggleTech(event) {
+  const type = event.target.dataset.type
+  const tech = technologies.find((t) => t.type === type)
+  tech.done = event.target.checked
+  
+  saveState()
+  init()
+}
+
+function openModal(html,title = APP_TITLE) {
+  document.title = `${title} | ${APP_TITLE}`
+  modal.innerHTML = html
   modal.classList.add('open')
 }
 
 function closeModal() {
+  document.title = APP_TITLE
   modal.classList.remove('open')
 }
 
@@ -74,10 +107,56 @@ function computeProgressPercent() {
 function toCard(tech) {
   const doneClass = tech.done ? 'done' : ''
   return `
-    <div class="card ${doneClass}">
-      <h3>${tech.title}</h3>
+    <div class="card ${doneClass}" data-type="${tech.type}">
+      <h3 data-type="${tech.type}">${tech.title}</h3>
     </div>
     `
+}
+
+function isInvalid(title, description) {
+  return !title.value || !description.value
+}
+
+function createTech(event) {
+  event.preventDefault()
+
+  // const title = event.target.title
+  // const description = event.target.description
+
+  const {title, description} = event.target
+
+  if (isInvalid(title, description)) {
+    if (!title.value) title.classList.add('invalid')
+    if (!description.value) description.classList.add('invalid')
+
+    setTimeout(() => {
+      title.classList.remove('invalid')
+      description.classList.remove('invalid')
+    }, 2000)
+    return
+  }
+
+  const newTech = {
+    title: title.value,
+    description: description.value,
+    done: false,
+    type: title.value.toLowerCase()
+  }
+
+  technologies.push(newTech)
+  title.value = ''
+  description.value = ''
+  saveState()
+  init()
+}
+
+function saveState() {
+  localStorage.setItem(LS_KEY, JSON.stringify(technologies))
+}
+
+function getState() {
+  const raw = localStorage.getItem(LS_KEY)
+  return raw ? JSON.parse(raw) : []
 }
 
 init()
